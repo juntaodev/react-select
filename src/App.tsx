54d9b3985ffa366b312a5b12d38f3a4a08871393
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2022-09-28 23:24:58 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Select } from "./Select";
+import { useState } from "react";
+
+const options = [
+  { label: "first", value: 1 },
+  { label: "second", value: 2 },
+  { label: "third", value: 3 },
+  { label: "fourth", value: 4 },
+  { label: "fifth", value: 5 },
+];
+
+function App() {
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+
+  return (
+    <>
+      <Select options={options} value={value} onChange={(o) => setValue(o)} />
+    </>
+  );
+}
+
+export default App;
